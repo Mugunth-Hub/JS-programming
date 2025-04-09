deb9c04dde2c1aec957582a00aee7bc1c73a0243
Mugunth-Hub <moorthimukundhan@gmail.com>
2025-04-10 00:41:26 +0530

    Add new file or changes

--- object.js
+++ object.js
@@ -13,9 +13,28 @@
 // let raceType = userProfile.eat();
 // console.log( raceType );
 
-let vehicle = {
-    "vehicle" : "4-wheeler",
-    "price": "34000",
-    fuelType : "petrol",
+// let vehicle = {
+//     "vehicle": "4-wheeler",
+//     "price": "34000",
+//     fuelType: "petrol",
+//     "seaterType": ["two", "three", "four"]
+// }
+// console.log(vehicle.vehicle);
+// console.log(vehicle["vehicle"]);
+// console.log(vehicle["fuelType"]);
+// console.log(vehicle["seaterType"][2]);
+
+//Shorthan Assigned Property
+//Dynamic Property 
+
+let UName = "mugu"
+let age = 23
+
+let dynamicProp = "employeeId"
+let person1 = {
+    UName ,
+    age ,
+    [dynamicProp] : "IFSC1779",
+    ["dynamicProp"] : "IFSC17779"
 }
-console.log( vehicle["fuelType"]);
+console.log(person1.UName,person1.age,person1.dynamicProp,person1[dynamicProp]);

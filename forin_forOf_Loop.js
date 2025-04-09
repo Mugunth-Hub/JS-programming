19160f7d5147c54f210fb305750c14fb91ef9f33
Mugunth-Hub <moorthimukundhan@gmail.com>
2025-04-10 00:57:17 +0530

    Add new file or changes

---
+++ forin_forOf_Loop.js
@@ -0,0 +1,22 @@
+let arr = [10, 20, 30, 40]
+
+// console.log(arr[0]);
+
+let length = arr.length
+
+// console.log(length);
+
+
+for (let i = 0; i < length; i++) {
+    console.log( arr[i] );
+
+}
+
+//String
+
+let str = "Javascript"
+
+for( let i=0; i<str.length; i++ ){
+    console.log(str[i]);
+    
+}

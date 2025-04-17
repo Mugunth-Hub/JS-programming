a8820347b63fe2f0208d412dfc0aa81014e4263b
Mugunth-Hub <moorthimukundhan@gmail.com>
2025-04-17 13:23:25 +0530

    Add new file or changes

---
+++ stringManipulate.js
@@ -0,0 +1,29 @@
+// ChartAt 
+
+// let str = "Hello World"
+// console.log(str.charAt(str.length - 1));
+
+// charCodeArt
+
+// let str1 = "Javascript"
+
+// console.log(str1.charCodeAt(3));
+
+// conCat
+
+// let newStr = str.concat(" ", str1)
+// console.log(newStr);
+
+// // Includes
+// let str2 = "Single Threaded"
+
+// console.log(str2.includes("i", 1));
+// console.log(str2.includes("j", 1));
+
+// IndexOf
+
+let newStr1 = " Mugunth "
+console.log(newStr1.indexOf("u",3));
+
+
+

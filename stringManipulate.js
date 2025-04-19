4db7a0418974cdcfe6af3fe4aa182fd14afe7cc1
Mugunth-Hub <moorthimukundhan@gmail.com>
2025-04-19 12:14:06 +0530

    Add new file or changes

--- stringManipulate.js
+++ stringManipulate.js
@@ -3,15 +3,18 @@
 // let str = "Hello World"
 // console.log(str.charAt(str.length - 1));
 
+
 // charCodeArt
 
 // let str1 = "Javascript"
 // console.log(str1.charCodeAt(3));
 
+
 // conCat
 
 // let newStr = str.concat(" ", str1)
 // console.log(newStr);
+
 
 // Include
 
@@ -19,10 +22,12 @@
 // console.log(str2.includes("i", 1));
 // console.log(str2.includes("j", 1));
 
+
 // IndexOf
 
 // let newStr1 = "Mugunth" // No value find output is -1
 // console.log(newStr1.indexOf("u"));  //  Left To Right 
+
 
 // LastIndexOf
 
@@ -43,6 +48,7 @@
 // console.log( str.replace("Mugunth","Mugu")); //only first
 // console.log( str.replaceAll("Mugunth","Mugu")); // All
 
+
 // Slice
 
 // let newStr = "Single Thread"
@@ -50,12 +56,71 @@
 // console.log( newStr.slice(3,7)); // start n end
 // console.log( newStr.slice(-3,-1)); // ( -3, -2)
 
+
 // SubString & SubStr
 
-let newStr1 = "Single Thread"
-console.log(newStr1.substring(3) );
-console.log(newStr1.substring(3,8) );
-console.log(newStr1.substring(-3) ); // Negative is only 0 so it will not make any change
+// let newStr1 = "Single Thread"
+// console.log(newStr1.substring(3) );
+// console.log(newStr1.substring(3,8) );
+// console.log(newStr1.substring(-3) ); // Negative is only 0 so it will not make any change
+
+
+// Split 
+
+// let words = "My Name is Mugu-Spatz"
+// console.log(words.split(""));
+// console.log(words.split(" "));
+// console.log(words.split(" ", 3));
+// console.log(words.split("-"));
+// console.log(words.split("-", 1));
+
+
+// Starts with
+
+// let words = "My Name is mugunth"
+// console.log(words.startsWith("M"));
+// console.log(words.startsWith("N ", 3));
+// console.log(words.startsWith("Name", 3));
+
+//endsWith 
+
+// let words1 = "My Name is mugunth"
+// console.log(words1.endsWith("h", words1.length));
+// console.log(words1.endsWith("is", 10));
+
+
+//toLowerCase
+
+// let words = "My Name is mugunth"
+// console.log(words.toLowerCase());
+
+
+//toUpperCase
+
+// let words = "My Name is mugunth"
+// console.log(words.toUpperCase());
+
+
+// trim 
+
+// let words = "My Name is mugunth" 
+// console.log(words.trim());
+
+
+// trimStart / trimLeft
+
+// let words = "My Name is mugunth"
+// console.log(words.trimStart());
+// console.log(words.trimLeft());
+
+
+// trimEnd / trimRight
+
+// let words = "My Name is mugunth"
+// console.log(words.trimEnd());
+// console.log(words.trimRight());
+
+
 
 
 

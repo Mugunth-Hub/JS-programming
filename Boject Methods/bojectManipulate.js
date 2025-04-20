f0c6760bf0147489b82c211785bd276f750ad954
Mugunth-Hub <moorthimukundhan@gmail.com>
2025-04-20 12:27:41 +0530

    Add new file or changes

---
+++ Boject Methods/bojectManipulate.js
@@ -0,0 +1,21 @@
+let person = {
+    pName : "Spatz",
+    pAge : 14
+}
+console.log(person );
+
+// Object.create
+
+// Object.assign
+
+// Object.entries
+
+// Object.fromEntries
+
+// Object.keys
+
+// Object.values
+
+// Object.freeze
+
+// Object.isFrozen

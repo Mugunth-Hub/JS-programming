460a2a7d494a63bd10a29c2b1f5cb29af57c8460
Mugunth-Hub <moorthimukundhan@gmail.com>
2025-04-21 18:31:53 +0530

    Add new file or changes

--- Deep_shallow_copy/deep_Shollow.js
+++ Deep_shallow_copy/deep_Shollow.js
@@ -20,23 +20,42 @@
 
 //Shallow Copy
 
-let person = {
-    name1: "Mugunth",
-    role: "under1000Pilot",
-    hobbies: {
-        cricket: "t20 player",
-        football: "60 mins",
+// let person = {
+//     name1: "Mugunth",
+//     role: "under1000Pilot",
+//     hobbies: {
+//         cricket: "t20 player",
+//         football: "60 mins",
 
-    }
+//     }
 
-}
-let person2 = {...person,hobbies:{...person.hobbies}}
-person.role = "pilot"
-person.hobbies.cricket = "ODI player "
-console.log(person,person2);
+// }
+// let person2 = {...person,hobbies:{...person.hobbies}}
+// person.role = "pilot"
+// person.hobbies.cricket = "ODI player "
+// console.log(person,person2);
 
-let person3 = JSON.stringify(person)
-console.log(person3);
+// let person3 = JSON.stringify(person)
+// let newobj = JSON.parse(person3)
+// console.log(newobj);
+
+//  //  Array - Shallow Copy
+
+let arr = [1, 2, 3, [4, 5, 6]]
+
+let arr1 = [...arr]
+
+//Deep Copy
+
+let newArr = JSON.parse(JSON.stringify(arr))
+
+arr[0] = "one"
+arr[3][0] = "four"
+newArr[3][1] = "Five"
+console.log(arr, arr1);
+console.log(newArr);
+
+
 
 
 

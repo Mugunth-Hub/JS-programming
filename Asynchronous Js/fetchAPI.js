5f40c13299f13097859fa36c17f9700846fa0f1a
Mugunth-Hub <moorthimukundhan@gmail.com>
2025-05-23 18:49:22 +0530

    Initial commit

--- Asynchronous Js/fetchAPI.js
+++ Asynchronous Js/fetchAPI.js
@@ -1,4 +1,12 @@
 fetch("")
-.then((response) =>{
-    return response.json();
-})
+    .then((response) => {
+        console.log(response.ok);
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.log(error.message);
+
+    })

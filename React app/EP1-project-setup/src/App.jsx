54924776cf7b5e064165ad0676044290b76b3072
Mugunth-Hub <moorthimukundhan@gmail.com>
2025-05-31 16:55:39 +0530

    Fix: added function and updated index.js

---
+++ React app/EP1-project-setup/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+let author = "Mugunthan"
+function App() {
+  return (
+    <div className="container">
+      <h1 style={{ backgroundColor: "red",color:"white"}}> React js</h1>
+      <label htmlfor= "user">UserName</label>
+      <input  ="button" />
+      <p> {author} </p>
+    </div>
+  )
+}
+
+export default App
